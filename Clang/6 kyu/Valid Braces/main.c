87d907d60866d879968f65b285077f51d2df85d7
Alexey Rubtsov <vostbur@yandex.ru>
2022-11-12 21:17:13 +0300

    Update main.c

--- Clang/6 kyu/Valid Braces/main.c
+++ Clang/6 kyu/Valid Braces/main.c
@@ -9,24 +9,16 @@ bool valid_braces(const char *braces)
     for (int i = 0; i < strlen(braces); i++)
     {
         if (braces[i] == '(' || braces[i] == '{' || braces[i] == '[')
-        {
             tracer[pointer++] = braces[i];
-        }
         else
         {
             if (pointer == 0)
-            {
                 return false;
-            }
             last_char = tracer[pointer - 1];
             if ((braces[i] == ')' && last_char == '(') || (braces[i] == '}' && last_char == '{') || (braces[i] == ']' && last_char == '['))
-            {
                 pointer--;
-            }
             else
-            {
                 break;
-            }
         }
     }
     return pointer == 0;

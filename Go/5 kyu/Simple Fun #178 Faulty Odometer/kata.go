17c1f93d3d8f2dd924cbd0b3c0a2be8b46818ce0
Alexey Rubtsov <vostbur@yandex.ru>
2022-11-13 20:47:37 +0300

    Update kata.go

--- Go/5 kyu/Simple Fun #178 Faulty Odometer/kata.go
+++ Go/5 kyu/Simple Fun #178 Faulty Odometer/kata.go
@@ -6,22 +6,12 @@ import (
 	"strings"
 )
 
-func Reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-func PowInt(x, y int) int {
-	return int(math.Pow(float64(x), float64(y)))
-}
-
 func FaultyOdometer(n int) (r int) {
 	digs := "012356789"
-	for i, v := range Reverse(fmt.Sprintf("%d", n)) {
-		r += strings.Index(digs, string(v)) * PowInt(9, i)
+	s := fmt.Sprintf("%d", n)
+	l := len(s) - 1
+	for i, v := range s {
+		r += strings.Index(digs, string(v)) * int(math.Pow(9, float64(l-i)))
 	}
 	return
 }
